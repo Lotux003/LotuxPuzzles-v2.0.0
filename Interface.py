07b0f17c695401e737f7ca2988f46ce24a566807
Lotux <155517834+Lotux003@users.noreply.github.com>
2024-11-23 23:40:19 -0600

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -7,7 +7,7 @@ def clear():
     os.system('cls')
 
 def spawn_program_and_die(program, exit_code=0):
-    subprocess.Popen(program)
+    subprocess.run(program)
     #sys.exit(exit_code)
 
 
@@ -22,11 +22,11 @@ def main():
     if choice == 1:
         Common.transition()
         #spawn_program_and_die(['python', 'Games/Maze.py'])
-        subprocess.Popen('python Games/Maze.py')
+        subprocess.run('python Games/Maze.py')
     if choice == 2:
         Common.transition()
         #spawn_program_and_die(['python', 'Games/TicTacToe.py'])
-        subprocess.Popen('python Games/TicTacToe.py')
+        subprocess.run('python Games/TicTacToe.py')
     if choice == 3:
         Common.transition()
         webbrowser.open('https://github.com/Lotux003')
